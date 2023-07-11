a82284a652285686c631e2fde9e12e32bada84cd
ramu siripalli <ramsiripalli2772@gmail.com>
2023-07-11 12:07:02 +0530

    Create BinarySearch.java
    Binary Search

---
+++ 14)Arrays/BinarySearch.java
@@ -0,0 +1,31 @@
+// Binary search
+
+public class Main {
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 10, 12, 14 };
+        int target = 14;
+
+        System.out.println("The target fount at index : " + binarySearch(arr, target));
+    }
+}
